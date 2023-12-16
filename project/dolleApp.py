9f29fd6e33dd362433c75e1cc6dc9c126a731ec3
George Kressler <gkressler@sandiego.edu>
2023-12-15 17:24:20 -0800

    test

--- project/dolleApp.py
+++ project/dolleApp.py
@@ -1,4 +1,16 @@
-import openai
+import openai 
 
-openai.apikey = open('apikey.txt','r').read()
+openai.api_key = open('apikey.txt','r').read()
 
+
+response = openai.images.generate(
+  model="dall-e-3",
+  prompt=(input("Please enter a prompt for Doll E: ")),
+  size="1024x1024",
+  quality="standard",
+  n=1,
+)
+
+image_url = response.data[0].url
+
+print(image_url)
